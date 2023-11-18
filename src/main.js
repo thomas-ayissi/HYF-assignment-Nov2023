7777c97a383134d8cb72a24f2507af7f6e290aca
Thomas Ayissi <thomas8sea@gmail.com>
2023-11-18 20:38:41 +0100

    some corrections

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
 // Add your JavaScript here
+let currentInput = '';
+
+function append(value) {
+    currentInput += value;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = '';
+    updateDisplay();
+}
+
+function deleteLast() {
+    currentInput = currentInput.slice(0, -1);
+    updateDisplay();
+}
+
+function calculateResult() {
+    try {
+        const result = eval(currentInput);
+        currentInput = result.toString();
+        updateDisplay();
+    } catch (error) {
+        currentInput = 'Error';
+        updateDisplay();
+    }
+}
+
+function updateDisplay() {
+    document.getElementById('result').value = currentInput;
+}
